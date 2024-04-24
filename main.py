0501a8d7168f54d0dd54c20cd428965a2ed99be5
mjesbar <miguedjba96@gmail.com>
2024-04-24 15:19:28 -0500

    feat: sst & tts capabilities

--- main.py
+++ main.py
@@ -1,5 +1,44 @@
 import os
+import ffmpeg
+import whisper
+from TTS.api import TTS
+
+
+def stt(name: str = 'voice', fmt: str = 'mp3') -> str:
+  NAME: str = name
+  FMT: str = fmt
+  print("STT: Recording...")
+  (
+    ffmpeg
+    .input('default', f='alsa', ar=48000)
+    .output(f'{NAME}.{FMT}', ar=48000, t=3.0)
+    .overwrite_output()
+    .run(quiet=True)
+  )
+  print("STT: Recording [OK]")
+  # · Processing the mp3 file to text
+  print("STT: Processing...")
+  model = whisper.load_model('base.en')
+  model.cuda()
+  ret = model.transcribe(f'{NAME}.{FMT}', temperature=0.8, language='en')
+  print("STT: Processing [OK]")
+  print("STT: Text:", ret['text'])
+  return ret['text']
+
+
+def ai():
+  pass
+
+
+def tts(text):
+  print("TTS: Synthesizing...")
+  tts = TTS(model_name="tts_models/en/jenny/jenny",
+            progress_bar=False, gpu=True)
+  tts.tts_to_file(text=text, file_path='synth.wav')
+  print("TTS: Synthesizing [OK]")
+  os.system('aplay synth.wav > /dev/null 2>&1')
 
 
 if __name__ == '__main__':
-    os.system('echo "Hello from main.py"')
+  text = stt()
+  tts(text)
